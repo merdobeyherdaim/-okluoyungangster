462d15fda319d81d8860306bf3e38c8ac2229d57
merdobeyherdaim <acamert23@gmail.com>
2025-05-08 13:26:13 +0300

    Create games.py

---
+++ games.py
@@ -0,0 +1,8 @@
+def tavla(update, context):
+    update.message.reply_text('Tavla başlatılıyor!')
+
+def satranc(update, context):
+    update.message.reply_text('Satranç başlatılıyor!')
+
+def oyunlar(update, context):
+    update.message.reply_text('Mevcut oyunlar: Tavla, Satranç')
